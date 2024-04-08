19509ef12d7a257b116cf1912dfc4de263811c5e
bhanukumari <117707378+bhanukumari@users.noreply.github.com>
2024-04-08 13:01:06 +0530

    Update variable.tf

--- terraform/php/variable.tf
+++ terraform/php/variable.tf
@@ -1,7 +1,13 @@
 variable "vpc_cidr1" {
   type = string
 
-  default = "10.0.0.0/16"
+  default = string
+}
+
+variable "vpc" {
+    type = string
+  #  default = "vpc_jenkins"
+    default = string
 }
 
 
